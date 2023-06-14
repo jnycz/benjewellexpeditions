26dac641338ad9db27e50d432c9d07695ef1fd3f
john nycz <nycz88@gmail.com>
2023-06-14 14:19:40 -0400

    Gtag config

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,9 +38,7 @@ module.exports = {
           // Setting this parameter is also optional
           respectDNT: true,
           // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          origin: "https://jewellexpeditions.com",
+          exclude: ["/admin/**", "/tags/**", "/blog/**"],
           // Delays processing pageview events on route update (in milliseconds)
           delayOnRouteUpdate: 0,
         },
