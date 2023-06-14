cf3d2b79ab40cf7ca2ba9fec5348aadc14d628c2
john nycz <nycz88@gmail.com>
2023-06-14 16:29:43 -0400

    Removed optimize id

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,13 +24,6 @@ module.exports = {
         trackingIds: [
           "G-58WYLVBMW7", // Google Analytics / GA
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "GTM-MX6DR84",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
